8c09a4928f1e897e32ee3a5c6a5705eaf5e5885c
AI Chad <minimalismconsulting@gmail.com>
2023-08-27 09:16:38 +1000

    Edits

--- own_db.py
+++ own_db.py
@@ -4,19 +4,19 @@ from chad_embedding import ChadEmbeddingFunc
 
 chad_emb = ChadEmbeddingFunc()
 
+
 add_data = False
-
-with open("scraped_database.json", 'r') as file:
-	data = json.load(file)
-
 if add_data:
+	with open("scraped_database.json", 'r') as file:
+		data = json.load(file)
+
 	for item in data:
 		chad_emb.add_entry(f"'{item['quote']}' - {item['author']}")
 
 
-text_check = "Working hard at a life project."
+text_check = "What do we do with our time?"
 top_scores_return = 5
 best_match, index_sorted = chad_emb.best_match(text_check, top_scores_return)
 
 for match in best_match:
-	print(match)
+	print(f"{match[0]} - {round(match[1], 3)}")
